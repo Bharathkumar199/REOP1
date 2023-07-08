b6c1c091b1a5853f982334bcf50c07af8661ac9f
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-07-08 15:53:15 +0530

    some changes

--- GIT__DEMO/src/main/java/GIT__PRACTISE/hello1.java
+++ GIT__DEMO/src/main/java/GIT__PRACTISE/hello1.java
@@ -5,5 +5,9 @@ public class hello1
  public static void main(String[] args) 
  { 
 	   System.out.println("hello world");
+	   System.out.println("hiii");
+	   int a=10;int b=20;
+	   int res =a+b;
+	   System.out.println(res);
  }
 }
