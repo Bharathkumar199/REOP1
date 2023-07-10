79147c54a930c966a9dce1bd834e49e1d6c7e50f
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-07-10 15:21:13 +0530

    just

---
+++ GIT__DEMO/src/main/java/GIT__PRACTISE/factor.java
@@ -0,0 +1,19 @@
+package GIT__PRACTISE;
+
+import java.util.Scanner;
+
+public class factor 
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the any number");
+		int n=sc.nextInt();
+		int fact=1;
+		for(int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+			
+		}
+		System.out.println("then given number of " + n + " factroial is " + fact);
+	}
+}
