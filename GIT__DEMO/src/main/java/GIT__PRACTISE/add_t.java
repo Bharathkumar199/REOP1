80c6e0aa8c00d2b5f40883030b940a5c67554345
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-07-10 13:10:32 +0530

    Create add_t.java

---
+++ GIT__DEMO/src/main/java/GIT__PRACTISE/add_t.java
@@ -0,0 +1,15 @@
+package GIT__PRACTISE;
+
+public class add_t 
+{
+	public static void main(String[] args) 
+	{
+		int a,b;
+		a=10;
+		b=20;
+		
+		int res =a+b;
+		System.out.println(res);
+		
+	}
+}
