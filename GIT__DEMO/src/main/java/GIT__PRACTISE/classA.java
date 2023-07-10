957702a5bf63e6e941c0ab17dc92eaa96620cf11
BHARATHKUAMR <bharathkumar.mech.rymec@gmail.com>
2023-07-10 17:41:37 +0530

    local10

---
+++ GIT__DEMO/src/main/java/GIT__PRACTISE/classA.java
@@ -0,0 +1,16 @@
+package GIT__PRACTISE;
+
+import java.util.Scanner;
+
+public class classA 
+{
+	  public static void main(String[] args) {
+		   Scanner sc=new Scanner(System.in);
+		   System.out.println("enter the value r");
+		   int r=sc.nextInt();
+			double pi=3.142;
+			double result=pi*r*r;
+			System.out.println("The value of circle is = "+ result);
+			sc.close();
+		}
+}
